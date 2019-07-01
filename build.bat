5cd748d978f212f35c0e2180f9c6ce8ffd5e2a67
happyZYM <happyzymak@qq.com>
2019-07-02 07:42:43 +0800

    restart

--- build.bat
+++
@@ -1,3 +0,0 @@
-cd C:\Users\zhuan_z6tlgsj\Documents\OI\Subjects\PyBuild
-pyinstaller -w -i C:\Users\zhuan_z6tlgsj\Documents\OI\pic\ico.ico -p C:\Users\zhuan_z6tlgsj\Documents\OI\pic -p C:\Users\zhuan_z6tlgsj\AppData\Local\Programs\Python\Python37-32\Lib -p C:\Users\zhuan_z6tlgsj\AppData\Local\Programs\Python\Python37-32\Lib\site-packages C:\Users\zhuan_z6tlgsj\Documents\OI\pic\upload.pyw
-pause
